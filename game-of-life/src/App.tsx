c034629db7c2853c6bdcb65122c05d864052d820
Nouman Syed <syednouman2114@gmail.com>
2022-08-07 20:27:25 +0530

    app.tsx creates a complete 50x50 grip

--- game-of-life/src/App.tsx
+++ game-of-life/src/App.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+
+const numRows = 50;
+const numCols = 50;
 
 const App: React.FC = () => {
-  return <div>YO</div>;
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+
+    return rows;
+  });
+
+  return ( 
+    <div 
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${numCols}, 15px)`
+      }}>
+      {grid.map((rows, i) => 
+        rows.map((col, k) => (
+          <div 
+            key={`${i}-${k}`}
+            style={{
+              width: 15, 
+              height: 15, 
+              backgroundColor: grid[i][k] ? "green" : "black",
+              border: "solid 0.5px white"
+            }} 
+          />
+        ))
+      )} 
+    </div>
+    );
 };
 
 export default App;
